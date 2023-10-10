7c88d931615d0578acb1f3c5d7f1a2bce3b30f0e
babu97 <benkaimugul@gmail.com>
2023-10-10 20:52:43 +0300

    finished the first tes

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  let left  = 0
+  let right = word.length- 1
+ while (left<right){
+
+   if(word[left] !== word[right]){
+    return false;
+   }
+   left ++;
+   right --;
 }
+return true;
+}
+isPalindrome('word')
 
 /* 
-  Add your pseudocode here
+variable left  = 0 
+variable right= last element of word
+
+while left <right
+ 
+if word.left != word.right
+ return false
+
+ right --
+ left ++
+
 */
 
 /*
   Add written explanation of your solution here
+    obtain the first and last char in the word and store them in the variable
+  loop through the word comparing the first char and the last char
+  check if the first character and the last character is the same
+  return flase if not the same
+  otherwise continuing itterating the remaining chars from left and right until it reaches the middle
+  return true if found 
+  otherwise exit
+
 */
 
 // You can run `node index.js` to view these console logs
